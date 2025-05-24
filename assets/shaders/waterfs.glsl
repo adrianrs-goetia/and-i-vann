3a7675d0f1b5fdaf7b18608bc3a07971d16b7c1a
adrianrs_goetia <adrian.r.strate@gmail.com>
2025-05-24 21:08:57 +0200

    shader values

--- assets/shaders/waterfs.glsl
+++ assets/shaders/waterfs.glsl
@@ -9,9 +9,8 @@ in vec3 fragNormal;
 out vec4 FragColor;
 
 vec4 oceanBlue = vec4(0.616, 0.929, 0.98, 1.0);
-float value = 0.8;
+float value = 0.91;
 
 void main() {
-    // FragColor = fragColor;
     FragColor = oceanBlue * value;
 }
